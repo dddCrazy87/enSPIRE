91462fbd0fa35dd6fe1716d514310d0e151707e3
dddCrazy87 <n126970@gmail.com>
2024-03-19 11:41:16 +0800

    Initial Commit

---
+++ enSPIRE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  enSPIRE
+//
+//  Created by 許昀韋 on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ enSPIRE/enSPIREApp.swift
@@ -0,0 +1,17 @@
+//
+//  enSPIREApp.swift
+//  enSPIRE
+//
+//  Created by 許昀韋 on 2024/3/19.
+//
+
+import SwiftUI
+
+@main
+struct enSPIREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
