bc2ce63d0ca2b4d2faaafc622a61aa86d9800427
Joanne <liuchengen>
2024-03-21 14:44:11 +0800

    create pinterest grid

--- enSPIRE/GalleryView.swift
+++ enSPIRE/GalleryView.swift
@@ -3,9 +3,17 @@ import SwiftUI
 
 struct GalleryView: View {
     var body: some View {
-        GridView()
+        var gridItems = [GridItem]()
+        for i in 0 ..< 50{
+            let randomHeight = CGFloat.random(in: 80 ... 400)
+            gridItems.append(GridItem(height: randomHeight, title: String(i)))
+        }
+        return ScrollView{
+            GridView(gridItems: gridItems, numberOfColumns: 2)
+        }
     }
 }
+    
 
 #Preview {
     GalleryView()
--- enSPIRE/GridView.swift
+++ enSPIRE/GridView.swift
@@ -1,18 +1,72 @@
 import SwiftUI
 
+struct GridItem: Identifiable {
+    var id = UUID()
+    
+    let height: CGFloat
+    let title: String
+}
+
 struct GridView: View {
-    var spacing: CGFloat = 10
+    struct Column: Identifiable {
+        let id = UUID()
+        var gridItems = [GridItem]()
+    }
+    
+    var columns = [Column]()
+    
+    let spacing: CGFloat
+    let horizontalPadding: CGFloat
+    
+    init(gridItems: [GridItem], numberOfColumns: Int, spacing: CGFloat = 10, horizontalPadding: CGFloat = 10){
+        self.spacing = spacing
+        self.horizontalPadding = horizontalPadding
+        
+        var columns = [Column]()
+        for _ in 0 ..< numberOfColumns{
+            columns.append(Column())
+        }
+        var columnsHeight = Array<CGFloat>(repeating: 0, count: numberOfColumns)
+        
+        for gridItem in gridItems {
+            var smallestColumnIndex = 0
+            var smallestHeight = columnsHeight.first!
+            for i in 1 ..< columnsHeight.count {
+                let curHeight = columnsHeight[i]
+                if curHeight < smallestHeight {
+                    smallestHeight = curHeight
+                    smallestColumnIndex = i
+                }
+            }
+            columns[smallestColumnIndex].gridItems.append(gridItem)
+            columnsHeight[smallestColumnIndex] += gridItem.height
+        }
+        self.columns = columns
+    }
+    
     var body: some View {
         HStack(alignment: .top, spacing: spacing){
-            LazyVStack(content: {
-                ForEach(1...10, id: \.self) { count in
-                    /*@START_MENU_TOKEN@*/Text("Placeholder \(count)")/*@END_MENU_TOKEN@*/
+            ForEach(columns){ column in
+                LazyVStack(spacing: spacing){
+                    ForEach(column.gridItems){ gridItem in
+                        Rectangle()
+                            .foregroundStyle(Color.blue)
+                            .frame(height: gridItem.height)
+                            .overlay {
+                                Text(gridItem.title)
+                                    .font(.system(size: 20, weight: .bold))
+                            }
+                    }
                 }
-            })
+            }
         }
+        .padding(.horizontal, horizontalPadding)
     }
 }
 
 #Preview {
-    GridView()
+    var gridItems = [GridItem]()
+    let randomHeight = CGFloat.random(in: 100 ... 400)
+    gridItems.append(GridItem(height: randomHeight, title: "0"))
+    return GridView(gridItems: gridItems, numberOfColumns: 2)
 }
