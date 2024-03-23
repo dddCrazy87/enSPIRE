1aa4d64f9247c94dd6e0405a020ede54a9fd21dd
Joanne <liuchengen>
2024-03-23 10:28:36 +0800

    add chat page layout

--- enSPIRE/ChatView.swift
+++ enSPIRE/ChatView.swift
@@ -9,7 +9,91 @@ import SwiftUI
 
 struct ChatView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("討論室")
+                .padding()
+                .font(.title)
+            ScrollView{
+                HStack{
+                    Spacer()
+                    Button{
+                        print("add")
+                    } label: {
+                        Text("新增討論")
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                    }
+                    .font(.system(size: 16))
+                    .foregroundStyle(Color.black)
+                    .background(Color.yellow)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Button{
+                        print("add")
+                    } label: {
+                        Text("跨領域討論")
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                    }
+                    .font(.system(size: 16))
+                    .foregroundStyle(Color.black)
+                    .background(Color.yellow)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                }
+                .padding(.vertical)
+                VStack{
+                    HStack{
+                        AnyShape(Circle())
+                            .frame(width: 50)
+                            .foregroundStyle(Color.yellow)
+                            .padding(.trailing)
+                        Text("使用者名稱")
+                        Spacer()
+                        AnyShape(Circle())
+                            .frame(width: 10)
+                            .foregroundStyle(Color.orange)
+                    }
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                    HStack{
+                        AnyShape(Circle())
+                            .frame(width: 50)
+                            .foregroundStyle(Color.yellow)
+                            .padding(.trailing)
+                        Text("使用者名稱")
+                        Spacer()
+                        AnyShape(Circle())
+                            .frame(width: 10)
+                            .foregroundStyle(Color.orange)
+                    }
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                    HStack{
+                        AnyShape(Circle())
+                            .frame(width: 50)
+                            .foregroundStyle(Color.yellow)
+                            .padding(.trailing)
+                        Text("使用者名稱")
+                        Spacer()
+                    }
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                    HStack{
+                        AnyShape(Circle())
+                            .frame(width: 50)
+                            .foregroundStyle(Color.yellow)
+                            .padding(.trailing)
+                        Text("使用者名稱")
+                        Spacer()
+                    }
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                }
+            }
+            .padding()
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
