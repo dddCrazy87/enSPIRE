0c574324d0104c6f14fd21f2f23dcbea7628deb6
dddCrazy87 <n126970@gmail.com>
2024-04-04 17:47:09 +0800

    modify upload pieces view and add upload photo function

--- enSPIRE/UploadPiecesView.swift
+++ enSPIRE/UploadPiecesView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
+import PhotosUI
 
 struct UploadPiecesView: View {
+    
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedPhotoData: Data?
     
     @State private var name = ""
     @State private var auther = ""
@@ -9,94 +13,102 @@ struct UploadPiecesView: View {
     
     var body: some View {
         
-        VStack(alignment: .leading) {
+        VStack {
             
-            Spacer()
-            HStack {
-                Spacer()
-                ZStack(alignment: .bottomTrailing) {
-                    Image("")
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
+
+                Rectangle()
+                    .frame(height: 250)
+                    .foregroundStyle(Color("YellowColor"))
+                
+                if let selectedPhotoData,
+                   let image = UIImage(data: selectedPhotoData) {
+                    Image(uiImage: image)
                         .resizable()
-                        .frame(width: 300, height: 200)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("上傳作品封面")
-                            .font(.system(size: 14))
-                            .foregroundColor(.black)
-                    }
-                    .background(Color.yellow)
-                    .cornerRadius(8)
-                    .padding()
-                    .buttonStyle(.borderedProminent)
-                    .tint(Color("Yellow2Color"))
+                        .frame(height: 250)
                 }
-                Spacer()
+                
+                PhotosPicker(selection: $selectedItem, matching: .any(of:[ .images, .not(.livePhotos)])) {
+                    Label("上傳作品封面", systemImage: "photo")
+                        .font(.system(size: 14))
+                        .foregroundColor(.black)
+                }
+                .tint(Color("Yellow2Color"))
+                .buttonStyle(.borderedProminent)
+                .cornerRadius(8)
+                .padding(10)
+                .onChange(of: selectedItem) { newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            selectedPhotoData = data
+                        }
+                    }
+                }
+                
             }
             
             
             HStack {
                 Text("作品名稱：")
-                    .frame(height: 30)
                     .bold()
                 TextField("作品名稱", text: $name)
             }
+            .padding(.leading, 30)
+            .padding(.top, 20)
             
             HStack {
                 Text("作者：")
-                    .padding(.leading)
                 TextField("作者", text: $auther)
             }
-            
-            Text("作品連結")
-                .frame(height: 30)
-                .bold()
+            .padding(.leading, 50)
             
             HStack {
-                Spacer()
-                TextField("在這裡貼上作品連結", text: $link)
-                    .frame(width: 300)
+                Text("作品連結")
+                    .bold()
                 Spacer()
             }
+            .padding(.top, 10)
+            .padding(.leading, 30)
             
-            Text("作品概述")
-                .frame(height: 30)
-                .bold()
+            TextField("在這裡貼上作品連結", text: $link)
+                .padding(.leading, 50)
             
             HStack {
-                Spacer()
-                TextField(
-                    "簡單介紹一下這個作品吧",
-                    text: $description,
-                    axis: .vertical
-                )
-                .frame(minHeight: 100, alignment: .top)
-                .multilineTextAlignment(.leading)
-                .foregroundStyle(.gray)
-                .padding()
-                .overlay{
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("YellowColor"), lineWidth: 2)
-                }
+                Text("作品概述")
+                    .bold()
                 Spacer()
             }
+            .padding(.top, 10)
+            .padding(.leading, 30)
+
+            TextField(
+                "簡單介紹一下這個作品吧",
+                text: $description,
+                axis: .vertical
+            )
+            .frame(minHeight: 100, alignment: .top)
+            .multilineTextAlignment(.leading)
+            .foregroundStyle(.gray)
+            .padding(5)
+            .overlay{
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color("YellowColor"), lineWidth: 2)
+            }
+            .padding(.leading, 30)
+            .padding(.trailing, 30)
             
             Spacer()
             
             Button{
-                
+                    
             }label: {
                 Text("上傳")
-                    .frame(width: 325, height: 30)
+                    .frame(width: 360, height: 30)
                     .foregroundColor(.black)
-                    
             }
             .buttonStyle(.borderedProminent)
             .tint(Color("Yellow2Color"))
         }
-        .frame(width: 350)
         .navigationTitle(name.count == 0 ? "未命名作品" : name)
         .navigationBarTitleDisplayMode(.inline)
     }
