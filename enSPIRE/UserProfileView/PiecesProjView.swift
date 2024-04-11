6d4ba9ee995afc933158952c4411a6cd37e89ea3
dddCrazy87 <n126970@gmail.com>
2024-04-11 11:26:55 +0800

    add navigation link to pieces proj view

--- enSPIRE/UserProfileView/PiecesProjView.swift
+++ enSPIRE/UserProfileView/PiecesProjView.swift
@@ -37,56 +37,14 @@ struct PiecesProjView: View {
                             .background(Color.white)
                             .cornerRadius(10)
                             
-                            ZStack {
-                                Rectangle()
-                                    .frame(width: 180, height: 260)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                                VStack {
-                                    Rectangle()
-                                        .frame(width: 120, height: 150)
-                                        .cornerRadius(10)
-                                        .foregroundColor(.gray)
-                                    
-                                    Text(piecesProj.pieces[index].title)
-                                        .padding(.top, 20)
-                                }
-                            }
+                            PiecesProj(piece: piecesProj.pieces[index])
                         }
                         
                         else if index % 2 == 1 {
-                            ZStack {
-                                Rectangle()
-                                    .frame(width: 180, height: 260)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                                VStack {
-                                    Rectangle()
-                                        .frame(width: 120, height: 150)
-                                        .cornerRadius(10)
-                                        .foregroundColor(.gray)
-                                    
-                                    Text(piecesProj.pieces[index].title)
-                                        .padding(.top, 20)
-                                }
-                            }
+                            PiecesProj(piece: piecesProj.pieces[index])
                             
                             if index + 1 < piecesProj.pieces.count {
-                                ZStack {
-                                    Rectangle()
-                                        .frame(width: 180, height: 260)
-                                        .foregroundColor(.white)
-                                        .cornerRadius(10)
-                                    VStack {
-                                        Rectangle()
-                                            .frame(width: 120, height: 150)
-                                            .cornerRadius(10)
-                                            .foregroundColor(.gray)
-                                        
-                                        Text(piecesProj.pieces[index+1].title)
-                                            .padding(.top, 20)
-                                    }
-                                }
+                                PiecesProj(piece: piecesProj.pieces[index+1])
                             }
                             else {
                                 Spacer().frame(width: 180)
@@ -112,4 +70,36 @@ struct PiecesProjView: View {
             Node(text: "女朋友的生日禮物", children: [Node(text: "4A")]),
             Node(text: "音樂比賽主題", children: [Node(text: "5A"),Node(text: "5AA"),Node(text: "5AAA"),Node(text: "5AAAA")])
         ]), piecesProj: PiecesProjs(pieces: [Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"), Pieces(title: "a", auther: "a", image: "a", description: "a", link: "a"), Pieces(title: "b", auther: "b", image: "b", description: "b", link: "b"), Pieces(title: "c", auther: "c", image: "c", description: "c", link: "c")]))
+}
+
+struct PiecesProj: View {
+    
+    let piece:Pieces
+    
+    var body: some View {
+        
+        NavigationLink {
+            PiecesView(piecesData: piece)
+        } label: {
+            ZStack {
+                Rectangle()
+                    .frame(width: 180, height: 260)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 120, height: 150)
+                            .cornerRadius(10)
+                            .foregroundColor(.gray)
+                        Text(piece.image)
+                            .foregroundColor(.black)
+                    }
+                    Text(piece.title)
+                        .padding(.top, 20)
+                        .foregroundColor(.black)
+                }
+            }
+        }
+    }
 }
