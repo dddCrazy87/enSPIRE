611d2d71930b9baf4ed4be66b77c7c75eb45bcd5
dddCrazy87 <n126970@gmail.com>
2024-04-11 12:01:28 +0800

    add navigation link to mind map proj

--- enSPIRE/ContentView.swift
+++ enSPIRE/ContentView.swift
@@ -14,19 +14,19 @@ struct ContentView: View {
     @State var curPage = PageController.mindmap
     
     // editing mind map proj
-    @State private var mindMap_editing = Node(text: "root")
+    private var mindMap_editing = Node(text: "root")
     
     // user data
-    @State private var userInfo = UserInfo(UserId: "111", userName: "Wayne", photo: "", job: ["job1", "job2"], habit: ["看電影", "看書"])
+    private var userInfo = UserInfo(UserId: "111", userName: "Wayne", photo: "", job: ["job1", "job2"], habit: ["看電影", "看書"])
     
-    @State private var mindMapProj = MindmapProjs(nodes: [
+    private var mindMapProj = MindmapProjs(nodes: [
         Node(text: "影像藝術論期中主題", children: [Node(text: "1A"),Node(text: "1AA"),Node(text: "1AAA")]),
         Node(text: "女朋友的生日禮物", children: [Node(text: "2A"),Node(text: "2AA"),Node(text: "2AA")]),
         Node(text: "推薦的晚餐", children: [Node(text: "3A"),Node(text: "3AAA")]),
         Node(text: "小說劇情", children: [Node(text: "4A")]),
         Node(text: "音樂比賽主題", children: [Node(text: "5A"),Node(text: "5AA"),Node(text: "5AAA"),Node(text: "5AAAA")])])
     
-    @State private var piecesPjoj = PiecesProjs(pieces: [Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"), Pieces(title: "a", auther: "a", image: "a", description: "a", link: "a"), Pieces(title: "b", auther: "b", image: "b", description: "b", link: "b"), Pieces(title: "c", auther: "c", image: "c", description: "c", link: "c")])
+    private var piecesPjoj = PiecesProjs(pieces: [Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"), Pieces(title: "a", auther: "a", image: "a", description: "a", link: "a"), Pieces(title: "b", auther: "b", image: "b", description: "b", link: "b"), Pieces(title: "c", auther: "c", image: "c", description: "c", link: "c")])
     
     var body: some View {
         NavigationStack {
@@ -51,7 +51,7 @@ struct ContentView: View {
                         .toolbarBackground(.visible, for: .bottomBar)
                         .toolbarColorScheme(.light, for: .bottomBar)
                 case .profile:
-                    UserProfileView(userInfo: userInfo, tabIndex: 0, mindmapProj: mindMapProj, piecesProj: piecesPjoj)
+                    UserProfileView(userInfo: userInfo, tabIndex: 0, mindmapProj: mindMapProj, piecesProj: piecesPjoj, mindMap_editing: mindMap_editing, curPage: $curPage)
                         .toolbarBackground(.visible, for: .bottomBar)
                         .toolbarColorScheme(.light, for: .bottomBar)
                 default:
--- enSPIRE/UserProfileView/MindMapProjView.swift
+++ enSPIRE/UserProfileView/MindMapProjView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct MindMapProjView: View {
     
     @ObservedObject var mindmapProj:MindmapProjs
+    @ObservedObject var mindMap_editing:Node
+    @Binding var curPage : ContentView.PageController
     
     var body: some View {
         
@@ -24,7 +26,10 @@ struct MindMapProjView: View {
                         if index == 0 {
                             
                             Button {
-                                
+                                mindMap_editing.text = "root"
+                                mindMap_editing.hintText = "root"
+                                mindMap_editing.children = []
+                                curPage = .mindmap
                             } label: {
                                 Image(systemName: "plus")
                                     .resizable()
@@ -35,29 +40,14 @@ struct MindMapProjView: View {
                             .background(Color.white)
                             .cornerRadius(10)
                             
-                            MindMapView(isPreview: true, rootNode: mindmapProj.nodes[index])
-                                .frame(width: 180, height: 240)
-                                .clipped()
-                                .padding(.vertical, 10)
-                                .background(Color.white)
-                                .cornerRadius(10)
+                            MindMapProj(node: mindmapProj.nodes[index], mindMap_editing: mindMap_editing, curPage: $curPage)
                         }
                         
                         else if index % 2 == 1 {
-                            MindMapView(isPreview: true, rootNode: mindmapProj.nodes[index])
-                                .frame(width: 180, height: 240)
-                                .clipped()
-                                .padding(.vertical, 10)
-                                .background(Color.white)
-                                .cornerRadius(10)
+                            MindMapProj(node: mindmapProj.nodes[index], mindMap_editing: mindMap_editing, curPage: $curPage)
                             
                             if index + 1 < mindmapProj.nodes.count {
-                                MindMapView(isPreview: true, rootNode: mindmapProj.nodes[index+1])
-                                    .frame(width: 180, height: 240)
-                                    .clipped()
-                                    .padding(.vertical, 10)
-                                    .background(Color.white)
-                                    .cornerRadius(10)
+                                MindMapProj(node: mindmapProj.nodes[index+1], mindMap_editing: mindMap_editing, curPage: $curPage)
                             }
                             else {
                                 Spacer().frame(width: 180)
@@ -74,13 +64,45 @@ struct MindMapProjView: View {
     }
 }
 
+struct MindMapProj: View {
+    
+    let node:Node
+    @ObservedObject var mindMap_editing:Node
+    @Binding var curPage : ContentView.PageController
+    
+    var body: some View {
+        
+        Button {
+            mindMap_editing.text = node.text
+            mindMap_editing.hintText = node.hintText
+            mindMap_editing.children = node.children
+            curPage = .mindmap
+        } label: {
+            MindMapView(isPreview: true, rootNode: node)
+                .frame(width: 180, height: 240)
+                .clipped()
+                .padding(.vertical, 10)
+                .background(Color.white)
+                .cornerRadius(10)
+        }
+        
+    }
+}
+
+struct MindMapProjTestView: View {
+    @State var curPage = ContentView.PageController.profile
+    var body: some View {
+        UserProfileView(userInfo: UserInfo(UserId: "1", userName: "桂林仔", photo: "", job: ["通緝犯", "黑道"], habit: ["擲筊", "請示", "開槍"]), tabIndex: 0, mindmapProj: MindmapProjs(nodes: [
+                Node(text: "影像藝術論期中主題", children: [Node(text: "1A"),Node(text: "1AA"),Node(text: "1AAA", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a")])])])])])]),
+                Node(text: "小說劇情", children: [Node(text: "2A"),Node(text: "2AA"),Node(text: "2AA")]),
+                Node(text: "推薦的晚餐", children: [Node(text: "3A"),Node(text: "3AAA")]),
+                Node(text: "女朋友的生日禮物", children: [Node(text: "4A")]),
+                Node(text: "音樂比賽主題", children: [Node(text: "5A"),Node(text: "5AA"),Node(text: "5AAA"),Node(text: "5AAAA")])
+            ]), piecesProj: PiecesProjs(pieces: [Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"), Pieces(title: "a", auther: "a", image: "a", description: "a", link: "a"), Pieces(title: "b", auther: "b", image: "b", description: "b", link: "b"), Pieces(title: "c", auther: "c", image: "c", description: "c", link: "c")]), mindMap_editing: Node(text: "root"), curPage: $curPage)
+    }
+}
 
 #Preview {
-    UserProfileView(userInfo: UserInfo(UserId: "1", userName: "桂林仔", photo: "", job: ["通緝犯", "黑道"], habit: ["擲筊", "請示", "開槍"]), tabIndex: 0, mindmapProj: MindmapProjs(nodes: [
-            Node(text: "影像藝術論期中主題", children: [Node(text: "1A"),Node(text: "1AA"),Node(text: "1AAA", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a", children: [Node(text: "a")])])])])])]),
-            Node(text: "小說劇情", children: [Node(text: "2A"),Node(text: "2AA"),Node(text: "2AA")]),
-            Node(text: "推薦的晚餐", children: [Node(text: "3A"),Node(text: "3AAA")]),
-            Node(text: "女朋友的生日禮物", children: [Node(text: "4A")]),
-            Node(text: "音樂比賽主題", children: [Node(text: "5A"),Node(text: "5AA"),Node(text: "5AAA"),Node(text: "5AAAA")])
-        ]), piecesProj: PiecesProjs(pieces: [Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"), Pieces(title: "a", auther: "a", image: "a", description: "a", link: "a"), Pieces(title: "b", auther: "b", image: "b", description: "b", link: "b"), Pieces(title: "c", auther: "c", image: "c", description: "c", link: "c")]))
+    MindMapProjTestView()
 }
+
