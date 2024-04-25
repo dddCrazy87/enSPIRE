440be4e4f2c7459c0dd93322f25c589974f26caa
Joanne0119 <liuchengen52033@gmail.com>
2024-04-25 10:57:05 +0800

    add click event to grid item in gallery view

--- enSPIRE/GalleryView/GalleryView.swift
+++ enSPIRE/GalleryView/GalleryView.swift
@@ -4,18 +4,18 @@ import SwiftUI
 struct GalleryView: View {
     
     let gridItems = [
-        GridItem(title: "img2"),
-        GridItem(title: "img7"),
-        GridItem(title: "img6"),
-        GridItem(title: "img4"),
-        GridItem(title: "img5"),
-        GridItem(title: "img11"),
-        GridItem(title: "img3"),
-        GridItem(title: "img10"),
-        GridItem(title: "img8"),
-        GridItem(title: "img9"),
-        GridItem(title: "img1"),
-        GridItem(title: "img12")
+        Pieces(title:"Ice Beer", auther: "Bing Image Creator", image: "img2", description: "realistic image of a cold glass of lemonade chilling in a pile of snow, polar bears in the background", link: ""),
+        Pieces(title:"", auther: "", image: "img7", description: "", link: ""),
+        Pieces(title:"", auther: "", image: "img6", description: "", link: ""),
+        Pieces(title:"Diamonds Bouquet", auther: "Bing Image Creator", image: "img4", description: "realistic image of a bouquet of flowers made of diamonds", link: "https://www.bing.com/images/create/realistic-image-of-a-bouquet-of-flowers-made-of-diamonds/1-410A7C6CCD7E4DD29D118B81BDF21C92?id=ofsZj%2FNICocjxNigLHOhWA%3D%3D&view=detailv2&idpp=genimg&FORM=GLPIDP&idpview=singleimage"),
+        Pieces(title:"野餐", auther: "劉丞恩", image: "img5", description: "晴空萬里的夏天午後，一起在花花草草的綠地上，享用野餐的悠閒日子", link: ""),
+        Pieces(title:"", auther: "", image: "img11", description: "", link: ""),
+        Pieces(title:"Fruits and Flowers", auther: "Bing Image Creator", image: "img3", description: "A picnic on a green meadow with a basket of fruits and flowers", link: "https://www.bing.com/images/create/A-picnic-on-a-green-meadow-with-a-basket-of-fruits-and-flowers/1-E66D534B73074E6DBB064EB19FC531AA?id=E393RMe6bjteIAMIYIE2jg%3D%3D&view=detailv2&idpp=genimg&FORM=GLPIDP&idpview=singleimage"),
+        Pieces(title:"", auther: "", image: "img10", description: "", link: ""),
+        Pieces(title:"", auther: "", image: "img8", description: "", link: ""),
+        Pieces(title:"積水的馬路", auther: "小恩", image: "img9", description: "雨後放晴的下午我與米子坐在馬路邊等待紅綠燈，踢著水、泡著腳、玩著水", link: ""),
+        Pieces(title:"Paper Animals", auther: "Bing Image Creator", image: "img1", description: "A nature reserve showing an elephant, zebra, and lion made of origami paper that have come to life", link: "https://www.bing.com/images/create/A-nature-reserve-showing-an-elephant%2C-zebra%2C-and-lion-made-of-origami-paper-that-have-come-to-life/1-737FD1EA2B1B4FB7872F219E7A8D859E?id=qgL23lMt2be3%2BD%2FSaB3h9A%3D%3D&view=detailv2&idpp=genimg&FORM=GLPIDP&idpview=singleimage"),
+        Pieces(title:"", auther: "", image: "img12", description: "", link: "")
     ]
     
     var body: some View {
@@ -25,8 +25,7 @@ struct GalleryView: View {
             }
             .padding()
         }
-        .navigationTitle("展覽室")
-        .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
     
--- enSPIRE/PiecesView.swift
+++ enSPIRE/PiecesView.swift
@@ -8,9 +8,9 @@ struct PiecesView: View {
         
         VStack {
             
-            Rectangle()
-                .frame(height: 250)
-                .foregroundStyle(Color("YellowColor"))
+            Image(piecesData.image)
+                .resizable()
+                .scaledToFit()
             
             ScrollView(.vertical) {
                 
@@ -72,6 +72,6 @@ struct PiecesView: View {
 
 #Preview {
     NavigationStack {
-        PiecesView(piecesData: Pieces(title: "萬有引力", auther: "牛頓", image: "image", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"))
+        PiecesView(piecesData: Pieces(title: "萬有引力", auther: "牛頓", image: "img2", description: "牛頓的萬有引力定律（英語：Newton's law of universal gravitation），通稱萬有引力定律，定律指出，兩個質點彼此之間相互吸引的作用力，是與它們的質量乘積成正比，並與它們之間的距離成平方反比。\n\n萬有引力定律是由艾薩克·牛頓稱之為歸納推理的經驗觀察得出的一般物理規律。它是古典力學的一部分，是在1687年於《自然哲學的數學原理》中首次發表的，並於1687年7月5日首次出版。當牛頓的書在1686年被提交給英國皇家學會時，羅伯特·虎克宣稱牛頓從他那裡得到了距離平方反比律。", link: "https://github.com/dddCrazy87/enSPIRE"))
     }
 }
